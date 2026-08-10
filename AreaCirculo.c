a9d16892abb0c2e21ad3cce6f7fb9aceeac0e49d
Pedro Bordini <pedrobordini.estudo@gmail.com>
2026-08-10 17:55:56 -0300

    Add AreaCirculo.c to calculate circle area

---
+++ AreaCirculo.c
@@ -0,0 +1,12 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        double raio = double.Parse(Console.ReadLine());
+        double area = 3.14159 * raio * raio;
+          Console.WriteLine("A=" + area.ToString("F4"));
+    }
+
+}
