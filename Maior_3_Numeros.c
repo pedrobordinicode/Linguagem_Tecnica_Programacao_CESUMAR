d799886c24aa10eef0c786ee4ffd5cb1cef0625d
Pedro Bordini <pedrobordini.estudo@gmail.com>
2026-09-08 20:26:19 -0300

    Add program to find the largest of three numbers

---
+++ Maior_3_Numeros.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	int a,b,c,temp;
+	printf("insira os valores de A,B,C: ");
+	scanf("%d %d %d",&a ,&b ,&c);
+	if (a>b && a>c){
+		printf("%d eh o maior\n", a);}
+	if(b>a && b>c){
+		printf("%d eh o maior\n", b);}
+	if(c>a && c>b){
+		printf("%d eh o maior\n", c);}
+	return 0;
+}
