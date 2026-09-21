6205190a5cb4d29c86af42fb7039c5b00bd8423d
Pedro Bordini <pedrobordini.estudo@gmail.com>
2026-09-21 15:48:18 -0300

    Add program to print integers in reverse order

---
+++ Listas_de_Atividade/Exercicio1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+ 
+int main() {
+
+    int a, b;
+ 
+    printf("Digite o primeiro numero inteiro: ");
+    scanf("%d", &a);
+    printf("Digite o segundo numero inteiro: ");
+    scanf("%d", &b);
+ 
+    printf("Ordem inversa: %d %d\n", b, a);
+  
+    return 0;
+}
