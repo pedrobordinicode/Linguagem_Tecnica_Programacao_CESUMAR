c4e19a4b15a911e709f96e1fb4c267fc06b584d4
Pedro Bordini <pedrobordini.estudo@gmail.com>
2026-09-21 15:53:16 -0300

    Refactor main function for temperature conversion

--- Convesao_C<->F.c
+++ Convesao_C<->F.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+int main() {
 	char escala;
 	float numero;
 	float conversao;
 	
 	printf("Para qual escala voce quer converter e qual o valor? \n(C)elsius ou (F)ahrenheit: "); /* pedir escala e analisar primeira letra */
 	scanf("%c %f" ,&escala,&numero);
-//	printf("%c\n", escala);
 
-	if (escala == 'f' || escala == 'F'){ // escala C > F
+	if (escala == 'f' || escala == 'F'){
  
 		conversao = ((numero*1.8)+32);
 		printf("valor de %.1fC em fahrenheit eh: %.1fF \n",numero, conversao);
 	} 
-	else if (escala == 'c' || escala == 'C'){ // escala F > C
+	else if (escala == 'c' || escala == 'C'){
 
 		conversao = ((numero-32)/1.8);
 		printf("o valor de %.1fF em celsius eh: %.1fC \n",numero, conversao);
